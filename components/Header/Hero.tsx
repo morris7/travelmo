95edf1631eb8c88d8bdddac4365223d30a9fd0d4
Sam Morris <sammorris7@hotmail.com>
2019-11-12 22:27:33 +0000

    update to hero html structure and styling

--- components/Header/Hero.tsx
+++ components/Header/Hero.tsx
@@ -5,9 +5,12 @@ import './Hero.scss';
 
 const Hero = () => (
   <section className="Hero relative flex">
-    <img src="/heroImage.jpg" />
-    <div className="Hero__centre-square border-4 border-white btn-4 ">
-      Travel Mo
+    <img src="/test.jpg" />
+    <div className="Hero__container inset-0">
+      <div className="Hero__tagline">Set yourself free!</div>
+      <div className="Hero__centre-square border-4 border-white btn-4 ">
+        Travel Mo
+      </div>
     </div>
   </section>
 )
