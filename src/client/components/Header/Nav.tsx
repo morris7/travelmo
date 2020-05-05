be2716de547033990c53f69dc9ac27dcd0555351
morris7 <sammorris7@hotmail.com>
2020-05-05 23:07:57 +0100

    add share button and update social media svgs

--- src/client/components/Header/Nav.tsx
+++ src/client/components/Header/Nav.tsx
@@ -26,7 +26,25 @@ const links: ILink[] = [
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
-})
+});
+
+let sharePage = () => { };
+
+if (process.browser) {
+  const navigator: any = window.navigator;
+
+  sharePage = () => {
+    if (navigator.share) {
+      navigator.share({
+        title: 'TravelMo',
+        text: 'Check out TravelMo travel blog.',
+        url: 'https://travelmo.co',
+      })
+        .then(() => console.log('shared successfully'))
+        .catch(() => console.log('failed to share'));
+    }
+  }
+}
 
 const Nav = (props: INavProps) => {
   const classes = classNames(ROOT_CLASSNAME, props.className);
@@ -63,11 +81,6 @@ const Nav = (props: INavProps) => {
       <nav className={`${ROOT_CLASSNAME}__nav--social-media`}>
         <ul className={`${ROOT_CLASSNAME}__list flex h-full`}>
           <li className={`${ROOT_CLASSNAME}__list-item`}>
-            <a href="#">
-              <svg id="logo-facebook" className="bs-icon" viewBox="0 0 512 512"><path d="M455.27 32H56.73A24.74 24.74 0 0032 56.73v398.54A24.74 24.74 0 0056.73 480H256V304h-53.55v-64H256v-51c0-57.86 40.13-89.36 91.82-89.36 24.73 0 51.33 1.86 57.51 2.68v60.43h-41.18c-28.12 0-33.48 13.3-33.48 32.9V240h67l-8.75 64h-58.25v176h124.6A24.74 24.74 0 00480 455.27V56.73A24.74 24.74 0 00455.27 32z"></path></svg>
-            </a>
-          </li>
-          <li className={`${ROOT_CLASSNAME}__list-item`}>
             <a href="https://www.instagram.com/morris7/">
               <svg id="logo-instagram" className="bs-icon" viewBox="0 0 512 512"><path d="M349.33 69.33a93.62 93.62 0 0193.34 93.34v186.66a93.62 93.62 0 01-93.34 93.34H162.67a93.62 93.62 0 01-93.34-93.34V162.67a93.62 93.62 0 0193.34-93.34h186.66m0-37.33H162.67C90.8 32 32 90.8 32 162.67v186.66C32 421.2 90.8 480 162.67 480h186.66C421.2 480 480 421.2 480 349.33V162.67C480 90.8 421.2 32 349.33 32z"></path><path d="M377.33 162.67a28 28 0 1128-28 27.94 27.94 0 01-28 28zM256 181.33A74.67 74.67 0 11181.33 256 74.75 74.75 0 01256 181.33m0-37.33a112 112 0 10112 112 112 112 0 00-112-112z"></path></svg>
             </a>
@@ -75,6 +88,11 @@ const Nav = (props: INavProps) => {
           <li className={`${ROOT_CLASSNAME}__list-item`}>
             <a href="#">
               <svg id="logo-rss" className="bs-icon" viewBox="0 0 512 512"><path d="M108.56 342.78a60.34 60.34 0 1060.56 60.44 60.63 60.63 0 00-60.56-60.44z"></path><path d="M48 186.67v86.55c52 0 101.94 15.39 138.67 52.11s52 86.56 52 138.67h86.66c0-151.56-125.66-277.33-277.33-277.33z"></path><path d="M48 48v86.56c185.25 0 329.22 144.08 329.22 329.44H464C464 234.66 277.67 48 48 48z"></path></svg>
+            </a>
+          </li>
+          <li className={`${ROOT_CLASSNAME}__list-item`}>
+            <a href="#" onClick={sharePage}>
+              <svg id="share-social-outline" className="bs-icon" viewBox="0 0 512 512"><circle cx="128" cy="256" r="48" strokeLinecap="round" strokeLinejoin="round"></circle><circle cx="384" cy="112" r="48" strokeLinecap="round" strokeLinejoin="round"></circle><circle cx="384" cy="400" r="48" strokeLinecap="round" strokeLinejoin="round"></circle><path strokeLinecap="round" strokeLinejoin="round" d="M169.83 279.53l172.34 96.94M342.17 135.53l-172.34 96.94"></path></svg>
             </a>
           </li>
         </ul>
--- src/client/components/Post/Post.tsx
+++ src/client/components/Post/Post.tsx
@@ -24,7 +24,7 @@ const Post: React.FunctionComponent<IPostProps> = ({ src, className }) => {
         <span className={`${ROOT_CLASSNAME}__text--small`}>Views: 89</span>
         <span className={`${ROOT_CLASSNAME}__text--small`}>Likes: 32</span>
         <p className={`${ROOT_CLASSNAME}__like-button`}>
-          <svg id="heart-outline" className="bs-icon" viewBox="0 0 512 512"><path d="M352.92 80C288 80 256 144 256 144s-32-64-96.92-64c-52.76 0-94.54 44.14-95.08 96.81-1.1 109.33 86.73 187.08 183 252.42a16 16 0 0018 0c96.26-65.34 184.09-143.09 183-252.42-.54-52.67-42.32-96.81-95.08-96.81z" stroke-linecap="round" stroke-linejoin="round" className="ionicon-fill-none ionicon-stroke-width"></path></svg>
+          <svg id="heart-outline" className="bs-icon" viewBox="0 0 512 512"><path d="M352.92 80C288 80 256 144 256 144s-32-64-96.92-64c-52.76 0-94.54 44.14-95.08 96.81-1.1 109.33 86.73 187.08 183 252.42a16 16 0 0018 0c96.26-65.34 184.09-143.09 183-252.42-.54-52.67-42.32-96.81-95.08-96.81z" strokeLinecap="round" strokeLinejoin="round" className="ionicon-fill-none ionicon-stroke-width"></path></svg>
         </p>
       </section>
     </article>
