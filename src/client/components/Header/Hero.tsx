bc15629998a6325e818c103457ad8cc6a9da3a9c
morris7 <sammorris7@hotmail.com>
2020-05-10 14:33:32 +0100

    small styling updates homepage carousel

--- src/client/components/Header/Hero.tsx
+++ src/client/components/Header/Hero.tsx
@@ -8,14 +8,14 @@ const Hero = () => (
   <section className="Hero relative flex">
 
     <Carousel className="Hero" >
-      <img src="/test.jpg" />  
+      <img src="/test.jpg" />
       <img src="/test2.jpg" />
     </Carousel>
-    
+
     <div className="Hero__container inset-0">
       <div className="Hero__tagline">Set yourself free!</div>
-      <div className="Hero__centre-square border-4 border-white btn-4 ">
-        Travel Mo
+      <div className="Hero__centre-square ">
+        TravelMo
       </div>
     </div>
   </section>
